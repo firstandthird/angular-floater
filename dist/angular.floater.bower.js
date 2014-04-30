3da78e5359f2e6320b74a1c92f6640ff442b6fff
Troy Whiteley <troy@somany.us>
2014-04-30 15:05:50 -0700

    Removed commented out lines

--- dist/angular.floater.bower.js
+++ dist/angular.floater.bower.js
@@ -18,37 +18,6 @@
           }
 
           $(el).floater(options);
-
-          // options.source = [];
-          // options.sourceKey = scope.sourceKey;
-          // options.allowOthers = scope.allowOthers;
-
-          // if (attrs.query) {
-          //   options.query = function(query, callback) {
-          //     return scope.query({ query: query, callback: callback });
-          //   };
-          // } else {
-          //   options.source = scope.source;
-          // }
-
-          // if (attrs.formatSuggestion) {
-          //   options.formatSuggestion = function(suggestion, value) {
-          //     return scope.formatSuggestion({ suggestion: suggestion, value: value });
-          //   };
-          // }
-
-          // var completeEl = $(el).complete(options);
-
-          // completeEl.on('complete:select', function(event, value){
-          //   if (!value) {
-          //     return;
-          //   }
-
-          //   scope.$apply(function(){
-          //     scope.selectedItem = value;
-          //   });
-
-          // });
         }
       };
     }]);
