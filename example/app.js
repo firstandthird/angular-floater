3b5afa8007fc65ea9ca091b87de15ead6dfdd35c
Troy Whiteley <troy@somany.us>
2014-04-30 16:04:12 -0700

    Callbacks for floatStart and floatEnd

--- example/app.js
+++ example/app.js
@@ -1,5 +1,13 @@
 angular.module('example-app', ['ftFloater']);
 
 var MainController = function($scope) {
+  $scope.startedFloating = function() {
+    $(el).addClass('floating');
+    console.log(this, 'float start');
+  };
 
+  $scope.stoppedFloating = function() {
+    $(this).addClass('floating');
+    console.log(this, 'float stop');
+  };
 };
